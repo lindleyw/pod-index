67ceb9f2d87551acf603e08e678ad00eae94d841
William Lindley <wlindley@wlindley.com>
2016-06-13 11:00:38 -0500

    Better formatting with Mojo::DOM. Localize all variables in lieu of full objects.

--- pod-index.pl
+++ pod-index.pl
@@ -17,12 +17,85 @@ binmode(STDOUT, ":utf8");
 #   CPAN::IndexPod
 #   Pod::Index
 
-#
+# TODO:
 # Probably ought to regard X<…> entities although metacpan doesn't create <a> for them.
 #
 
 # Original one-liner (sh)
 # grep -r head2 . | perl -n -e 'chomp; s/:=head./:/; s{\./}{}; my($fil,$head)=split /:/;$href=$head;$href=~s/^\s//;$href=~s/\s/-/g;$fil=~s{\..+}{}g;$fil=~s{/}{::}g;print "$head: <a href=https://metacpan.org/pod/$fil#$href>$fil</a><br>\n"' | sort | uniq >/tmp/f.html
+
+################
+
+{
+    # Maps local filename to manpage name.  Based on the =head1 NAME value.
+    my %file_manpage;
+
+    # All defined references.
+    my %references;
+
+    sub save_definition ($in_file, $refname, $display_name = $refname) {
+        $references{$display_name}{strip_extension($in_file)} = 
+          convert_to_href_text($refname);
+    }
+
+    sub save_file_manpage ($filename, $manpage) {
+        $file_manpage{strip_extension($filename)} = $manpage;
+    }
+
+    sub manpages_list {
+        return values %file_manpage;
+    }
+
+    sub manpage_get {
+        return $file_manpage{$_[0]};
+    }
+
+    sub references_list {
+        keys %references;
+    }
+
+    sub reference_get {
+        return $references{$_[0]};
+    }
+
+    { 
+        my $current_file;
+        my $current_heading;
+        my $save_next_text_as_module_name = 0;
+
+        sub parse_entity ($element, $attrs, @subnodes) {
+            if ($element =~ /^head(\d)/) {
+                my $level = $1;
+                $current_heading = plaintext_of(@subnodes);
+                if ($level == 1 && (lc($current_heading) eq 'name')) {
+                    $save_next_text_as_module_name = 1;
+                } elsif ($level == 2) {
+                    save_definition ( $current_file, $current_heading );
+                }
+            } elsif ($save_next_text_as_module_name && $element =~ /^para$/i ) {
+                $save_next_text_as_module_name = 0;
+                $current_heading = plaintext_of(@subnodes);
+                $current_heading =~ m/^\s*(\S+)/;
+                my $firstword = $1;
+                save_file_manpage($current_file, $firstword);
+            }
+        }
+
+        sub parse_document ($parse_file, $element_name, $attrs, @entities) {
+            # called with root node of a POD document
+            $current_heading = undef;
+            $current_file = $parse_file;
+            $save_next_text_as_module_name = 0;
+            foreach my $ent (@entities) {
+                parse_entity (@{$ent});
+            }
+        }
+    }
+}
+
+################
+
+# Retrieve plaintext from POD object
 
 sub plaintext_of (@subnodes) {
     my $accreted_text = '';
@@ -43,8 +116,6 @@ sub strip_extension ($filename) {
     return $filename;
 }
 
-################
-
 sub convert_to_href_text ($human_text) {
     $human_text =~ s/(\s|\(|=|\[)/-/g;
     $human_text =~ s/([^a-zA-Z0-9_\-*:])//g;
@@ -57,89 +128,40 @@ sub convert_local_path_to_href ($file) {
     return $file;
 }
 
-# Evil globals.
-
-# Maps local filename to manpage name.  Based on the =head1 NAME value.
-my %file_manpage;
-
-# All defined references.
-my %references;
-
-sub save_definition ($in_file, $refname, $display_name = $refname) {
-    $references{$display_name}{strip_extension($in_file)} = 
-      convert_to_href_text($refname);
-}
-
-sub save_file_manpage ($filename, $manpage) {
-    $file_manpage{strip_extension($filename)} = $manpage;
-}
-
-{ 
-    my $current_file;
-    my $current_heading;
-    my $save_next_text_as_module_name = 0;
-
-    sub parse_entity ($element, $attrs, @subnodes) {
-        if ($element =~ /^head(\d)/) {
-            my $level = $1;
-            $current_heading = plaintext_of(@subnodes);
-            if ($level == 1 && (lc($current_heading) eq 'name')) {
-                $save_next_text_as_module_name = 1;
-            } elsif ($level == 2) {
-                save_definition ( $current_file, $current_heading );
-            }
-        } elsif ($save_next_text_as_module_name && $element =~ /^para$/i ) {
-            $save_next_text_as_module_name = 0;
-            $current_heading = plaintext_of(@subnodes);
-            $current_heading =~ m/^\s*(\S+)/;
-            my $firstword = $1;
-            save_file_manpage($current_file, $firstword);
-        }
-    }
-
-    sub parse_document ($parse_file, $element_name, $attrs, @entities) {
-        # called with root node of a POD document
-        $current_heading = undef;
-        $current_file = $parse_file;
-        $save_next_text_as_module_name = 0;
-        foreach my $ent (@entities) {
-            parse_entity (@{$ent});
-        }
-    }
-}
+################
 
 sub clean_heading ($original) {
     # Clean headings for index display
     $original =~ s/^\s+//;
     $original =~ s/\smean\?$//;
     if ($original =~ m/^((?:(?:who|what|when|where|which|why|how|is|are|a|an|do|does|don't|doesn't|can|not|I|need|to|about|did|my|the|there)\s+|error\s+"|message\s+")+)(.*)$/i) {
-	my ($prefix, $main) = ($1, ucfirst($2));
-	$main =~ s/[?"]//g;
-	# $prefix =~ s/[?"]//g;
-	return $main;
+        my ($prefix, $main) = ($1, ucfirst($2));
+        $main =~ s/[?"]//g;
+        # $prefix =~ s/[?"]//g;
+        return $main;
     }
     # Nibbling the carrot -> Carrot, nibbling the
     if ($original =~ m/^(\w+ing(?:\s+and\s+\w+ing))\s+(a|an|the|some|any|all|to|from|your)?\b\s*(.*)$/) {
-	my ($verb, $qualifier, $remainder) = ($1, $2, $3);
-	$qualifier ||= '';
-	# print ucfirst("$remainder, $verb $qualifier\n");
-	return ucfirst("$remainder, $verb $qualifier");
+        my ($verb, $qualifier, $remainder) = ($1, $2, $3);
+        $qualifier ||= '';
+        # print ucfirst("$remainder, $verb $qualifier\n");
+        return ucfirst("$remainder, $verb $qualifier");
     }
     # $variable=function_name(...) -> function_name
     if ($original =~ m/^[\$@]\w+\s*=\s*(?:\$\w+\s*->\s*)?(\w+)/) {
-	return $1;
+        return $1;
     }
     # $variable->function_name(...) -> function_name
     if ($original =~ m/^\$\w+\s*->\s*(\w+)/) {
-	return $1;
+        return $1;
     }
     # Module::Module->function_name(...) -> function_name
     if ($original =~ m/^\w+(?:::\w+)+\s*->\s*(\w+)/) {
-	return $1;
+        return $1;
     }
     # function_name($args,...) -> function_name
     if ($original =~ m/^(\w+)\s*\(\s*[\$@%]\w+/) {
-	return $1;
+        return $1;
     }
     # ($var, $var) = function_name(...) -> function_name
     if ($original =~ m/^\([\$@%][^)]+\)\s*=\s*(?:\$\w+\s*->\s*)?(\w+)/) {
@@ -147,6 +169,8 @@ sub clean_heading ($original) {
     }
     return $original;
 }
+
+################
 
 # Accepts a list of module names.  e.g., "Mojo Mojolicious"
 # From this get a list of filenames in the Perl directory
@@ -192,10 +216,10 @@ foreach my $parse_file (@file_list) {
     parse_document($parse_file, @{$pod->root});
 }
 
-foreach my $r (sort {fc($a) cmp fc($b)} keys %references) {
+foreach my $r (sort {fc($a) cmp fc($b)} references_list()) {
     my $new = clean_heading($r);
     if ($new ne $r) {
-	foreach my $orig_file (keys %{$references{$r}}) { 
+	foreach my $orig_file (keys %{reference_get($r)}) { 
 	    save_definition( $orig_file, $r, $new);
 	}
     }
@@ -210,40 +234,49 @@ my $dom = Mojo::DOM->new("<!DOCTYPE html><head></head><body></body></html>");
 $dom->at('head')->append_content('<title>Cross-Reference</title>');
 
 $dom->at('body')->append_content('<h2>Perl Manpage Index</h2>');
-$dom->at('body')->append_content('<p id="pod-listing">Covers the following:</p>');
-$dom->at('body')->append_content('<dl id="heading-listing"></dl>');
+$dom->at('body')->append_content('<div id="coverage"><p id="pod-listing">Covers the following:</p></div>');
+$dom->at('body')->append_content('<div id="thumbs"></div>');
+$dom->at('body')->append_content('<div id="contents"></div>');
+$dom->at('#coverage')->append_content('<dl id="heading-listing"></dl>');
 
-foreach my $m (sort values %file_manpage) {
-    if (index($m, '::') < 0) {
+# List of indexed pages
+
+foreach my $m (sort (manpages_list())) {
+    if (index($m, '::') < 0) {  # major module heading
         $dom->at('#pod-listing')->append_content('<br />');
     }
     $dom->at('#pod-listing')->append_content(qq(<a href="https://metacpan.org/pod/$m">$m</a> ));
 }
-$dom->at('#pod-listing')->append_content('<br /><hr />');
+$dom->at('#pod-listing')->append_content(qq(<br />\n<hr />\n));
 
-my @headings = sort {fc($a) cmp fc($b)} keys %references;
+# Jump-to-thumb tabs
+
+my @headings = sort {fc($a) cmp fc($b)} references_list();
 my %thumbs;
 for (@headings) {
     $thumbs{uc(substr($_,0,1))}++;
 }
-$dom->at('body')->append_content( join(' ', map {qq(<a href="#head_$_">$_</a>) } (sort keys %thumbs)) . "\n");
+$dom->at('#thumbs')->append_content( join(' ', map {qq(<a href="#head_$_">$_</a>) } (sort keys %thumbs)) . "\n");
 
-foreach my $r (@headings) {
-    my $heading = substr($r,0,1); # First character
-    my $under_heading = $dom->at("#$heading ~dl");
-    if (!defined $under_heading) {
-        $dom->at('body')->append_content(qq(<h3 id="$heading">$heading</h3><dl>\n));
-        $DB::single = 1;
-        $under_heading = $dom->at("#$heading ~dl");
+# Alphabetical list of headings, with thumb tabs
+
+foreach my $heading (@headings) {
+    my $tab = uc(substr($heading,0,1)); # First character
+    my $under_tab = $dom->at("#head_$tab dl");
+    if (!defined $under_tab) {
+        $dom->at('#contents')
+          ->append_content(qq(\n<hr width="50%" />\n<div id="head_$tab" style="-moz-columns: 20em 2;"><h3>$tab</h3><dl></dl></div>\n));
+        $under_tab = $dom->at("#head_$tab dl");
     }
-    $under_heading->append_content("<dt>$r</dt>");
+    $under_tab->append_content("<dt>$heading</dt>");
     my @sources;
-    foreach my $orig_file (sort {fc($a) cmp fc($b)} keys %{$references{$r}}) { 
-	my $manpage = $file_manpage{$orig_file};
+    foreach my $orig_file (sort {fc($a) cmp fc($b)} keys %{reference_get($heading)}) { 
+	my $manpage = manpage_get($orig_file);
         next unless defined $manpage;
-	push @sources, qq(<a href="https://metacpan.org/pod/${manpage}#$references{$r}{$orig_file}">$manpage</a>);
+        my $r = reference_get($heading);
+	push @sources, qq(<a href="https://metacpan.org/pod/${manpage}#$r->{$orig_file}">$manpage</a>);
     }
-    $under_heading->append_content( '<dd>'.join(', ', @sources).'</dd>' );
+    $under_tab->append_content( '<dd>'.join(', ', @sources).'</dd>' );
 }
 
 print $dom;
